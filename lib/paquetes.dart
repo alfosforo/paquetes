0376b12bbe40d1502ae758bf766ec9a9b48d7c2f
alfosforo <alfonso.contreras.cc@gmail.com>
2020-08-29 01:26:16 -0400

    Trying country api response

--- lib/paquetes.dart
+++ lib/paquetes.dart
@@ -1,5 +1,9 @@
 import 'package:http/http.dart' as http;
-import 'package:paquetes/classes/request_response.dart';
+import 'package:paquetes/classes/currency.dart';
+import 'package:paquetes/classes/language.dart';
+
+import 'classes/country.dart';
+import 'classes/request_response.dart';
 
 void getRequestResponseService() {
   final url = 'https://reqres.in/api/users?page=2';
@@ -9,4 +13,26 @@ void getRequestResponseService() {
     print('per_page: ${resReq.perPage}');
     print('id del tercero: ${resReq.data[2].id}');
   });
+}
+
+void getCountry() {
+  final url = 'https://restcountries.eu/rest/v2/alpha/cl';
+  http.get(url).then((res) {
+    final country = countryFromJson(res.body);
+    print('===========================');
+    print('Pais: ${country.name}');
+    print('Población: ${country.population}');
+    print('Fronteras:');
+    country.borders.forEach((border) =>
+        print('    ${border[0]}${border.substring(1).toLowerCase()}'));
+
+    print('Idiomas:');
+    country.languages.forEach((lang) => print('    ${lang.nativeName}'));
+    print('Latitud: ${country.latlng[0]}');
+    print('Longitud: ${country.latlng[1]}');
+    print('Monedas:');
+    country.currencies.forEach((currency) => print('    ${currency.name}'));
+    print('Bandera: ${country.flag}');
+    print('===========================');
+  }).catchError((error) => print);
 }
